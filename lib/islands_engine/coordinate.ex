672e3552371bb2b89d804a70eeccd209397fd542
alexaxms <alexaxmss@hotmail.com>
2019-05-01 21:07:02 -0400

    Functional Web Development With Elixir 1749

--- lib/islands_engine/coordinate.ex
+++ lib/islands_engine/coordinate.ex
@@ -7,6 +7,6 @@ defmodule IslandsEngine.Coordinate do
   def new(row, col) when row in(@board_range) and col in(@board_range), do:
     {:ok, %Coordinate{row: row, col: col}}
 
-  def new(_row, _col), do: {:invalid, :invalid_coordinate}
+  def new(_row, _col), do: {:error, :invalid_coordinate}
 
 end
